88c3ede2fd02f7ea90d08dfbbf8bf3562af1778f
Ekaitz10 <61252620+Ekaitz10@users.noreply.github.com>
2020-03-12 09:32:15 +0100

    Update EmpresaG6.sql

--- Bases EmpresaG6 - Ekaitz/EmpresaG6.sql
+++ Bases EmpresaG6 - Ekaitz/EmpresaG6.sql
@@ -18,6 +18,7 @@ Matricula VARCHAR(10) NOT NULL PRIMARY KEY,
 num_Puertas INTEGER(1),
 capacidad_Maletero_dm3 INTEGER(3),
 FOREIGN KEY (Matricula) REFERENCES Vehiculos(Matricula)
+ON DELETE CASCADE
 );
 
 CREATE TABLE Camiones(
@@ -25,6 +26,7 @@ Matricula VARCHAR(10) NOT NULL PRIMARY KEY,
 carga_kg INTEGER(5),
 tipo_Mercancia VARCHAR(1) NOT NULL,
 FOREIGN KEY (Matricula) REFERENCES Vehiculos(Matricula)
+ON DELETE CASCADE
 );
 
 CREATE TABLE Cliente(
@@ -34,6 +36,7 @@ Matricula_Vehiculo_Vendido VARCHAR(10) NOT NULL,
 Nombre VARCHAR(15),
 TLFContacto INTEGER(9),
 Correo VARCHAR(10)
+FOREIGN KEY (Matricula_Vehiculo_Vendido) REFERENCES Vehiculos(Matricula)
 );
 
 CREATE TABLE Series(
@@ -56,7 +59,9 @@ serie VARCHAR(10) NOT NULL,
 Tipo_Vechiculo VARCHAR(6)
 );
 
--- TRIGGERS
+-- TRIGGERS E INSERTS
+
+-- creo el trigger para rellenar Historial a la vez que Vehiculos
 
 drop trigger if exists RellenarHistorial;
 create trigger RellenarHistorial
@@ -64,18 +69,16 @@ AFTER INSERT ON Vehiculos for each row
 insert into Historial(Matricula, Fecha_compra, num_Bastidor, color, num_Asientos, precio, serie, Tipo_vehiculo)
 values (new.Matricula, new.Fecha_compra, new.num_Bastidor, new.color, new.num_Asientos, new.precio, new.serie, new.Tipo_vehiculo);
 
+INSERT INTO Vehiculos VALUES("234 FDA",'2020-06-03',"V23P52E93","rojo",2,380000,"LAMB0001","Coche");
 
--- PROCESO QUE SEGUIRÁ UN VEHICULO
--- Registro:
+-- creo el siguiente trigger para actualizar el color cuando se actualice en vehiculos.
 
-INSERT INTO Vehiculos VALUES("234 FDA","2020-06-03","V23P52E93","Rojo","2","380000","LAMB0001","Coche");
+drop trigger if exists ActualizarHistorial;
+create trigger ActualizarHistorial
+AFTER UPDATE ON Vehiculos for each row
+update historial SET color = new.color;
+
+UPDATE Vehiculos SET color = "morado" WHERE Matricula = "234 FDA";
+
 INSERT INTO Coches VALUES("234 FDA","2","240");
 INSERT INTO Series VALUES("LAMB0001","Lamborghini","Aventador","2014");
--- Se cambia el color:
-
-CALL CambioColor();
-
--- 	LLAMANDO AL PROCESO ALMACENADO
--- realiza una consulta sobre el color del coche indicado
-
-CALL ProcesoColor();
